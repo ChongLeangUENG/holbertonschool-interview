0bb76e0967da978309c56b7bcdfae6db44a320eb
ChongLeangUENG <leangcnrp777@gmail.com>
2023-08-09 20:48:59 +0200

    Check the box is locked or unlocked

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -3,23 +3,24 @@
 method that determines if all the boxes can be opened.
 """
 
+
 def canUnlockAll(boxes):
-    """ Initialize a list to keep track of the visited boxes """
+    # Initialize a list to keep track of the visited boxes
     visited = [False] * len(boxes)
-    
-    """ The first box (box 0) is unlocked initially """
+
+    # The first box (box 0) is unlocked initially
     visited[0] = True
-    
-    """ Initialize a stack for DFS """
+
+    # Initialize a stack for DFS
     stack = [0]
-    
-    """ Perform DFS """
+
+    # Perform DFS
     while stack:
         current_box = stack.pop()
         for key in boxes[current_box]:
             if not visited[key]:
                 visited[key] = True
                 stack.append(key)
-    
-    """ Check if all boxes have been visited """
+
+    # Check if all boxes have been visited
     return all(visited)
