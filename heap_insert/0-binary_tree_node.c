99f059fcf2532119b5bb49b48c05fdfd937ae937
ChongLeangUENG <leangcnrp777@gmail.com>
2023-08-28 13:41:28 +0200

    Create a new node

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -8,16 +8,15 @@
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *NewNode = malloc(sizeof(binary_tree_t));
-
-    if (NewNode == NULL)
+    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+    
+    if (!new_node)
         return NULL;
 
-    if (parent != NULL)
-    {
-        NewNode->parent = parent;
-    }
-    NewNode->n = value;
+    new_node->n = value;
+    new_node->parent = parent;
+    new_node->left = NULL;
+    new_node->right = NULL;
 
-    return NewNode;
+    return new_node;
 }
