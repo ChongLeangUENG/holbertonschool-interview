fbaaebede11baf5845777ed97b93e6152588803a
ChongLeangUENG <leangcnrp777@gmail.com>
2023-09-11 15:06:58 +0200

    Function to compute the sum of two sandpiles

---
+++ sandpiles/0-sandpiles.c
@@ -0,0 +1,88 @@
+#include "sandpiles.h"
+
+/**
+ * is_stable - checks if sandpile is stable.
+ * @grid: grid to check
+ * Return: 1 if stable else 0
+ */
+int is_stable(int grid[3][3])
+{
+    int i, j;
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (grid[i][j] > 3)
+                return (0);
+        }
+    }
+    return (1);
+}
+
+/**
+ * topple - topples the sandpile to distribute sand
+ * @grid: grid to topple
+ */
+void topple(int grid[3][3])
+{
+    int i, j;
+    int copy[3][3];
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            copy[i][j] = grid[i][j];
+        }
+    }
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (copy[i][j] > 3)
+            {
+                grid[i][j] -= 4;
+                if (i - 1 >= 0) grid[i - 1][j]++;
+                if (i + 1 < 3) grid[i + 1][j]++;
+                if (j - 1 >= 0) grid[i][j - 1]++;
+                if (j + 1 < 3) grid[i][j + 1]++;
+            }
+        }
+    }
+}
+
+/**
+ * sandpiles_sum - compute the sum of two sandpiles.
+ * @grid1: first sandpile
+ * @grid2: second sandpile
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+    int i, j;
+    
+    // Sum grid1 and grid2 into grid1
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            grid1[i][j] += grid2[i][j];
+        }
+    }
+
+    while (!is_stable(grid1))
+    {
+        printf("=\n");
+        for (i = 0; i < 3; i++)
+        {
+            for (j = 0; j < 3; j++)
+            {
+                if (j)
+                    printf(" ");
+                printf("%d", grid1[i][j]);
+            }
+            printf("\n");
+        }
+        topple(grid1);
+    }
+}
