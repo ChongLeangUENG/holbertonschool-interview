cccdddce1c946645e00aa40a76ea4ae3874ea04e
ChongLeangUENG <leangcnrp777@gmail.com>
2023-09-26 16:39:32 +0200

    Log Parsing using Python

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 import sys
 
 # Initialize variables
@@ -8,10 +7,12 @@ status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
 count = 0
 size = 0
 
+
 def print_statistics(size, codes):
     print("File size: {}".format(size))
     for key in sorted(codes):
         print("{}: {}".format(key, codes[key]))
+
 
 try:
     for ln in sys.stdin:
