f75a257e6fed408098674c7a25de068dad9a0854
Chong Leang UENG <chong-tony@Chongs-MacBook-Air.local>
2023-10-30 17:27:57 +0100

    function that slides and merges an array of integers

---
+++ slide_line/slide_line.h
@@ -0,0 +1,12 @@
+#ifndef __SLIDE_LINE_H__
+#define __SLIDE_LINE_H__
+
+#define SLIDE_LEFT  1
+#define SLIDE_RIGHT 2
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int slide_line(int *line, size_t size, int direction);
+
+#endif
