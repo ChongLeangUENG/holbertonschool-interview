585b37d81865ca995a7570fa25d5f1b0eac54124
Chong Leang UENG <chong-tony@Chongs-MacBook-Air.local>
2023-11-02 08:45:28 +0100

    Fix merge not goes to right side

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -39,38 +39,39 @@ int slide_line(int *line, size_t size, int direction)
                 line[i + 1] = 0;
             }
         }
-
-        for (i = 0, j = 0; i < size; i++)
-        {
-            if (line[i] != 0)
-            {
-                if (j != i)
-                {
-                    line[j] = line[i];
-                    line[i] = 0;
-                }
-                j++;
-            }
-        }
     }
     else
     {
-        for (i = size - 1, j = size - 1; i > 0; i--)
-        {
-            if (line[i] != 0)
-            {
-                j = i - 1;
-                while (j < size - 1 && line[j + 1] == 0)
-                {
-                    line[j + 1] = line[j];
-                    line[j] = 0;
-                    j++;
+        for (i = size - 1, j = size - 1; j < size; i--) {
+            if (line[i] != 0) {
+                if (j != i) {
+                    line[j] = line[i];
+                    line[i] = 0;
                 }
-                if (line[i] == line[j])
-                {
-                    line[i] *= 2;
-                    line[j] = 0;
+                j--;
+            }
+            if (i == 0) {
+                break;
+            }
+        }
+
+        for (i = size - 1; i > 0; i--) {
+            if (line[i] == line[i - 1]) {
+                line[i] *= 2;
+                line[i - 1] = 0;
+            }
+        }
+
+        for (i = size - 1, j = size - 1; j < size; i--) {
+            if (line[i] != 0) {
+                if (j != i) {
+                    line[j] = line[i];
+                    line[i] = 0;
                 }
+                j--;
+            }
+            if (i == 0) {
+                break;
             }
         }
     }
