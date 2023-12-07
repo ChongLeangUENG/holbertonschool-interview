9bc212f6f6c7a9e45df32e1bbe52db24aa72efed
HBTN-IDF-S7812 <chong-tony@Chongs-MacBook-Air.local>
2023-12-07 14:51:37 +0100

    Add readme and function that get api starwar

---
+++ starwars_api/0-starwars_characters.js
@@ -0,0 +1,43 @@
+#!/usr/bin/node
+
+const fetch = require('node-fetch');
+
+const getStarWarsCharacters = async (movieId) => {
+    try {
+        // Base URL for the Star Wars API
+        const baseUrl = 'https://swapi.dev/api';
+
+        // Fetch film data
+        const filmResponse = await fetch(`${baseUrl}/films/${movieId}/`);
+        
+        if (!filmResponse.ok) {
+            throw new Error('Error fetching film data');
+        }
+
+        const filmData = await filmResponse.json();
+
+        // Fetch and print each character's name
+        for (const characterUrl of filmData.characters) {
+            const characterResponse = await fetch(characterUrl);
+
+            if (!characterResponse.ok) {
+                console.error('Error fetching character data');
+                continue;
+            }
+
+            const characterData = await characterResponse.json();
+            console.log(characterData.name);
+        }
+    } catch (error) {
+        console.error(error.message);
+    }
+};
+
+// Get movie ID from command line argument
+const movieId = process.argv[2];
+
+if (movieId) {
+    getStarWarsCharacters(movieId);
+} else {
+    console.log('Please provide a movie ID as a command line argument.');
+}
