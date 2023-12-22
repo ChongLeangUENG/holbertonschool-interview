93ad62632e6ff13a1a04feffbd0341537d5ea766
HBTN-IDF-S7812 <chong-tony@Chongs-MacBook-Air.local>
2023-12-22 08:53:51 +0100

    Fix C style to fit with Betty

--- heap_sort/0-heap_sort.c
+++ heap_sort/0-heap_sort.c
@@ -5,7 +5,8 @@
  * @a: first integer
  * @b: second integer
  */
-void swap(int *a, int *b) {
+void swap(int *a, int *b)
+{
 int tmp = *a;
 *a = *b;
 *b = tmp;
@@ -19,12 +20,14 @@ int tmp = *a;
  * @end: end index of the heap
  * @size: size of the array
  */
-void sift_down(int *array, size_t start, size_t end, size_t size) {
+void sift_down(int *array, size_t start, size_t end, size_t size)
+{
 size_t root = start;
 size_t child;
 size_t to_swap;
 
-while ((child = 2 * root + 1) <= end) {
+while ((child = 2 * root + 1) <= end)
+{
 to_swap = root; /* Keeps track of child to swap with */
 
 if (array[to_swap] < array[child])
@@ -32,9 +35,12 @@ to_swap = child;
 if (child + 1 <= end && array[to_swap] < array[child + 1])
 to_swap = child + 1;
         
-if (to_swap == root) { /* If root holds max element */
+if (to_swap == root)
+{ /* If root holds max element */
 return;
-} else {
+}
+else
+{
 swap(&array[root], &array[to_swap]);
 print_array(array, size);
 root = to_swap; /* Follow the node to keep heapifying */ 
@@ -47,10 +53,12 @@ root = to_swap; /* Follow the node to keep heapifying */
  * @array: array to be heapified
  * @size: size of the array
  */
-void heapify(int *array, size_t size) {
+void heapify(int *array, size_t size)
+{
 ssize_t start = (size - 2) / 2; /*  Last parent node */
 
-while (start >= 0) {
+while (start >= 0)
+{
 sift_down(array, start, size - 1, size);
 start--;
 }
@@ -62,17 +70,20 @@ start--;
  * @array: array to sort
  * @size: size of the array
  */
-void heap_sort(int *array, size_t size) {
+void heap_sort(int *array, size_t size)
+{
 size_t end;
 
-if (size < 2) {
+if (size < 2)
+{
 return;
 }
 
 heapify(array, size);
 
 end = size - 1;
-while (end > 0) {
+while (end > 0)
+{
 swap(&array[end], &array[0]);
 print_array(array, size);
 end--;
