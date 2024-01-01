e2bfd79b4d19abc454f6fb7bd3b980ead7381752
HBTN-IDF-S7812 <chong-tony@chongs-air.home>
2024-01-01 13:01:28 +0100

    Updates style

--- advanced_binary_search/0-advanced_binary.c
+++ advanced_binary_search/0-advanced_binary.c
@@ -1,19 +1,20 @@
 #include "search_algos.h"
-#include <stdio.h>
 
 /**
  * print_array - Prints an array of integers
  * @array: The array to be printed
  * @size: The size of the array
  */
-void print_array(int *array, size_t size) {
-    printf("Searching in array: ");
-    for (size_t i = 0; i < size; i++) {
-        printf("%d", array[i]);
-        if (i < size - 1)
-            printf(", ");
-    }
-    printf("\n");
+void print_array(int *array, size_t size) 
+{
+size_t i;
+printf("Searching in array: ");
+for (i = 0; i < size; i++) {
+printf("%d", array[i]);
+if (i < size - 1)
+printf(", ");
+}
+printf("\n");
 }
 
 /**
@@ -24,32 +25,25 @@ void print_array(int *array, size_t size) {
  * @value: The value to search for
  * @return: The index where value is located or -1
  */
-int binary_search_rec(int *array, size_t left, size_t right, int value) {
-    if (right >= left) {
-        size_t mid = left + (right - left) / 2;
+int binary_search_rec(int *array, size_t left, size_t right, int value) 
+{
+if (right >= left) {
+size_t mid = left + (right - left) / 2;
 
-        // Print current subarray
-        print_array(array + left, right - left + 1);
+print_array(array + left, right - left + 1);
 
-        // Check if the middle element is the value
-        if (array[mid] == value) {
-            // Check if it's the first occurrence
-            if (mid == left || array[mid - 1] != value)
-                return mid;
-            else
-                return binary_search_rec(array, left, mid - 1, value);
-        }
+if (array[mid] == value) {
+if (mid == left || array[mid - 1] != value)
+return mid;
+else
+return binary_search_rec(array, left, mid - 1, value);
+}
 
-        // If the value is smaller than mid, search in the left subarray
-        if (array[mid] > value)
-            return binary_search_rec(array, left, mid - 1, value);
-
-        // Otherwise, the value is in the right subarray
-        return binary_search_rec(array, mid + 1, right, value);
-    }
-
-    // Value not found
-    return -1;
+if (array[mid] > value)
+return binary_search_rec(array, left, mid - 1, value);
+return binary_search_rec(array, mid + 1, right, value);
+}
+return -1;
 }
 
 /**
@@ -59,9 +53,10 @@ int binary_search_rec(int *array, size_t left, size_t right, int value) {
  * @value: The value to search for
  * @return: The index where value is located or -1
  */
-int advanced_binary(int *array, size_t size, int value) {
-    if (array == NULL || size == 0)
-        return -1;
-    
-    return binary_search_rec(array, 0, size - 1, value);
+int advanced_binary(int *array, size_t size, int value)
+{
+if (array == NULL || size == 0)
+return -1;
+
+return binary_search_rec(array, 0, size - 1, value);
 }
