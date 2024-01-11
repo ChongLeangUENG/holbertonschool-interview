22ce0cea72a97a1086b59c0879c26ed1ec9dbda0
HBTN-IDF-S7812 <chong-tony@Chongs-MacBook-Air.local>
2024-01-11 13:49:50 +0100

    Script that count api

---
+++ count_it/0-count.py
@@ -0,0 +1,43 @@
+import requests
+import sys
+from collections import Counter
+
+
+def count_words(subreddit, word_list, after='', counter=None):
+    if counter is None:
+        counter = Counter()
+    headers = {'User-Agent': 'Keyword Counter Bot 0.1'}
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100&after={after}"
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        return
+
+    data = response.json()
+    posts = data.get('data', {}).get('children', [])
+    if not posts:
+        return
+
+    for post in posts:
+        title = post['data']['title'].lower()
+        for word in word_list:
+            word = word.lower()
+            counter[word] += title.count(f' {word} ')
+
+    after = data['data']['after']
+    if after is not None:
+        count_words(subreddit, word_list, after, counter)
+    else:
+        for word, count in sorted(
+                counter.items(), key=lambda item: (-item[1], item[0])):
+            if count > 0:
+                print(f'{word}: {count}')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print(f"Usage: {sys.argv[0]} <subreddit> <list of keywords>")
+        print(f"Ex: {sys.argv[0]} programming 'python java javascript'")
+    else:
+        count_words(sys.argv[1], sys.argv[2].split())
